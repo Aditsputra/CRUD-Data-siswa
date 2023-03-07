6b6c05e2350ecb7db148113e8dfe87fde77f0ecf
AditSputra <aphocodelight@gmail.com>
2023-03-07 22:20:33 +0700

    docs:Menambah Dokumentasi Koneksi Database

--- connection.php
+++ connection.php
@@ -1,8 +1,9 @@
 <?php 
 
+//Koneksi Ke Database
 $connect = mysqli_connect('localhost', 'Admin', 'Admin12.,3##', 'Data_siswa');
 
-
+//Pengecekan
 if($connect === false){
     die("ERROR: Database Tidak Terkoneksi" . mysqli_connect_error());
 }
